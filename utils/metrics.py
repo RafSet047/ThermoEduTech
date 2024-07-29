257feb5aca7f155f0562bae5f6ec020cbf2c5813
Rafayel Setyan <rsetyan3@gatech.edu>
2024-07-29 20:38:58 +0400

    added MAPE in the metrics calculations

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,6 +1,6 @@
 from typing import Iterable, Dict 
 import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
 
 def regression_report(y_true: Iterable, y_pred: Iterable) -> Dict:
     """
@@ -16,11 +16,13 @@ def regression_report(y_true: Iterable, y_pred: Iterable) -> Dict:
     mse = mean_squared_error(y_true, y_pred)
     rmse = np.sqrt(mse)
     r2 = r2_score(y_true, y_pred)
+    mape = mean_absolute_percentage_error(y_true, y_pred)
 
     report = {
         'Mean Absolute Error (MAE)': float(mae),
         'Mean Squared Error (MSE)': float(mse),
         'Root Mean Squared Error (RMSE)': float(rmse),
+        "Mean Absolute Percentage Error (MAPE)": float(mape),
         'R2 Score': float(r2)
     }
     return report
